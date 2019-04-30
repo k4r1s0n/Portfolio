ae327628ed0ead720ea56f9534f0392704b02578
Alex <ale8under8@gmail.com>
2019-04-30 08:33:27 -0700

    change navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
 import React from 'react';
@@ -5,7 +6,7 @@ import React from 'react';
 function Navbar() {
   return (
     <div className="Navbar">
-      <nav className="navbar navbar-default" role="navigation">
+      <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div className="container-fluid">
           <div className="navbar-header">
             <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -13,7 +14,7 @@ function Navbar() {
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <a className="navbar-brand" href="#">ALEX IBRAGIMOV</a>
+            <a className="navbar-brand" href="#"><i className="fas fa-battery-full" id="battery"></i> Alex Ibragimov</a>
           </div>
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul className="nav navbar-nav navbar-right">
@@ -23,11 +24,11 @@ function Navbar() {
               <li className="dropdown">
                 <a href="#" className="dropdown-toggle" data-toggle="dropdown">Contact Me<b className="caret"></b></a>
                 <ul className="dropdown-menu">
-                  <li><a href="#">GitHub</a></li>
-                  <li><a href="#">LinkedIn</a></li>
-                  <li><a href="#">Email</a></li>
+                  <li><a href="https://github.com/k4r1s0n" target="_blank"><i className="fab fa-github-square"></i> GitHub</a></li>
+                  <li><a href="https://www.linkedin.com/in/alex-i-045b00134/" target="_blank"><i className="fab fa-linkedin"></i> LinkedIn</a></li>
+                  <li><a href="mailto:oleksandr.ibrahimov@gmail.com" target="_blank"><i className="fas fa-envelope-square"></i> Email</a></li>
                   <li className="divider"></li>
-                  <li><a href="#">Phone Number</a></li>
+                  <li><a href="#"><i className="fas fa-mobile-alt"></i> Phone Number</a></li>
                 </ul>
               </li>
             </ul>
