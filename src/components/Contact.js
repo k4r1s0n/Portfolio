728299120ab9bf7c042a46578528949ad06dc359
Alex <ale8under8@gmail.com>
2019-05-27 08:22:09 -0700

    delete contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,62 +1,15 @@
 import React, { Component } from 'react'
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import axios from 'axios';
 
 
 export default class Contact extends Component {
   constructor() {
     super()
 
-    this.state = {
-      name: '',
-      email: '',
-      message: ''
-    }
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  async handleSubmit(e) {
-    e.preventDefault()
-
-    const { name , email, message} = this.state;
-
-    const form = await axios.post('/api/form',{
-      name,
-      email,
-      message
-    })
-  }
+    
 
   render() {
     return (
       <div style={{marginTop: 200,}} className="container">
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Group>
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="text" name="name" onChange={this.handleChange} />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" name="email" onChange={this.handleChange} />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Message</Form.Label>
-            <Form.Control as="textarea" rows="5" name="message" onChange={this.handleChange} />
-          </Form.Group>
-          
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
       </div>
     )
   }
