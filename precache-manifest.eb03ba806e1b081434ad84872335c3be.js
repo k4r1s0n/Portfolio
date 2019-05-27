b101df4410a09886f72dff7122328e6dddf6ca56
Alex <ale8under8@gmail.com>
2019-05-27 08:41:00 -0700

    Updates

---
+++ precache-manifest.eb03ba806e1b081434ad84872335c3be.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae51393c2e3b3e759098737b538ba0e6",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "0e0ecd3530d080aa0696",
+    "url": "/Portfolio/static/css/2.344a6b9d.chunk.css"
+  },
+  {
+    "revision": "d9f79385c5eac98c959c",
+    "url": "/Portfolio/static/css/main.40a196ab.chunk.css"
+  },
+  {
+    "revision": "0e0ecd3530d080aa0696",
+    "url": "/Portfolio/static/js/2.d8aaa708.chunk.js"
+  },
+  {
+    "revision": "d9f79385c5eac98c959c",
+    "url": "/Portfolio/static/js/main.36c52533.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "7c425582fa4e1a5e8efd2bf490bf8d6e",
+    "url": "/Portfolio/static/media/Areas of Knoledge PDF.7c425582.pdf"
+  },
+  {
+    "revision": "9f25f13c43a5f4fceb59c29ac622dd6d",
+    "url": "/Portfolio/static/media/Facetune.9f25f13c.jpg"
+  },
+  {
+    "revision": "5403314f7539b2fd5b494a5eadc636e7",
+    "url": "/Portfolio/static/media/Mountains.5403314f.jpg"
+  },
+  {
+    "revision": "203e4b2aca8b95e8fe1594c6072b0565",
+    "url": "/Portfolio/static/media/MyLogoSm.203e4b2a.png"
+  },
+  {
+    "revision": "22fdb194ef56902377737ec27913ad22",
+    "url": "/Portfolio/static/media/drum-machine.22fdb194.png"
+  },
+  {
+    "revision": "7327e5df4f86a0e5e2d55458fa557ffe",
+    "url": "/Portfolio/static/media/javascript-calculator.7327e5df.png"
+  },
+  {
+    "revision": "06067492a1a0d5ffc69606ae03d57cb1",
+    "url": "/Portfolio/static/media/markdown-previewer.06067492.png"
+  },
+  {
+    "revision": "482568801510146b320897ce4a10be5a",
+    "url": "/Portfolio/static/media/pomodoro-clock.48256880.png"
+  },
+  {
+    "revision": "3f7d7463073106dde3bff1b4c65177d4",
+    "url": "/Portfolio/static/media/quote-machine.3f7d7463.png"
+  },
+  {
+    "revision": "8d0306dc94bb0ccc4e40efba34456f55",
+    "url": "/Portfolio/static/media/weather-app.8d0306dc.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f89bc77ae4660988e2c6fcbf660a0196.js"
+  "/Portfolio/precache-manifest.eb03ba806e1b081434ad84872335c3be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
