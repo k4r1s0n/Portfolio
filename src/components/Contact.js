66a848a113af03f52b1759cfdc51ec6ae0acf66d
Alex <ale8under8@gmail.com>
2019-05-27 08:22:46 -0700

    delete contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react'
 
 
 export default class Contact extends Component {
-  constructor() {
-    super()
-
-    
-
+  
   render() {
     return (
       <div style={{marginTop: 200,}} className="container">
