7961e7f0e223f3cf906ae185c93b46df10e23ef2
Alex <ale8under8@gmail.com>
2019-05-29 18:49:44 -0700

    styling correction

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -150,7 +150,7 @@ export default class Projects extends Component {
         <Spring
           config={{ duration: 2000 }}
           from={{opacity:0, marginTop: -1000}}
-          to={{opacity:1, marginTop: 120}}>
+          to={{opacity:1, marginTop: 130}}>
           {props => (
             <div style={props}>
               <div id="slider" className="container">
